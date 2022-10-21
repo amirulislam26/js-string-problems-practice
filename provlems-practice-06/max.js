e7eb5e49fb28bedda6246fb0d065d11c49a7b3ad
amirulislam26 <26techamirulislam@gmail.com>
2022-10-21 11:23:14 -0700

    provlem 06 max js added

---
+++ provlems-practice-06/max.js
@@ -0,0 +1,18 @@
+const jim = 84;
+const dela = 65;
+if(jim > dela){
+    console.log('jim will get the cake')
+}
+else{
+    console.log('dela will you get the cake')
+}
+
+//////////////..............
+const myLove = 84;
+const yourLove = 100;
+if(myLove > yourLove){
+    console.log('MyLove will get the cake')
+}
+else{
+    console.log('yourLove will you get the cake')
+}
